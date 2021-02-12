f8e38c495398e603747a93903e5c61503c6ae314
Michael Schlottke-Lakemper <michael@sloede.com>
2021-02-12 06:25:09 +0100

    Add install_reactions_verbatim

--- src/KROME.jl
+++ src/KROME.jl
@@ -1,7 +1,32 @@
 module KROME
 
 using Reexport: @reexport
+using CBinding: 𝐣𝐥
 
 include("libkrome.jl")
+
+export install_reactions_verbatim
+
+
+"""
+    install_reactions_verbatim(dir=".")
+
+Copy the file `reactions_verbatim.dat` from the package-internal KROME build directory to the
+directory specified in `dir` and return the filename of the newly created file. If the target file
+alreay exists, it will be overwritten.
+"""
+function install_reactions_verbatim(dir=".")
+  filename = "reactions_verbatim.dat"
+  source_path = joinpath(pathof(@__MODULE__) |> dirname |> dirname, "deps", "krome", "build", filename)
+  target_path = joinpath(dir, filename)
+
+  if !isfile(source_path)
+    error("File '$source_path' does not exist. Have you successfully built KROME yet?")
+  end
+
+  cp(source_path, target_path, force=true, follow_symlinks=true)
+
+  return target_path
+end
 
 end
